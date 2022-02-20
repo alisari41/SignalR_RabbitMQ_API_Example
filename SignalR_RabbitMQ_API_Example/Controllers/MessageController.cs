77c96a631a535e7fb4bf5f9a5d69433f33b8f339
alisari41 <alisari41@outlook.com>
2022-02-21 00:33:03 +0300

    Add project files.

---
+++ SignalR_RabbitMQ_API_Example/Controllers/MessageController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace SignalR_RabbitMQ_API_Example.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {//Client'tan gelen mesajı karşılayıp RabbitMQ'ya göndericez
+
+        [HttpPost("{message}")]
+        public IActionResult Post(string message)
+        {
+
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://zreirkoo:80Fa-japcFjVWS7-kBG-joOyXDszAZw-@chimpanzee.rmq.cloudamqp.com/zreirkoo");//oluşturduğum cloudAMQP adresini veriyorum
+            using IConnection connection = factory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+            channel.QueueDeclare("messagequeue", false, false, false);
+            //kuyruğa verdiğim isim. Durable=RabbitMQ da mesaj kalıcı mı kalıcak yoksa resetlenicek mi
+            //Exclusive = Birden fazla kanalın bu kuyruğa bağlanıp bağlanamayacağını belirtiyoruz.
+
+
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish("", "messagequeue", body: data);
+
+            return Ok();
+        }
+
+    }
+}
